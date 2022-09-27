d8ef4c4d182299da1a0cd0c4438c0093ea164ca9
Leo Febey <overclockedamiga@gmail.com>
2022-09-27 23:41:43 +1000

    Now with OwnCast and responsive design yay

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const fs = require('fs');
 const settings_json = fs.readFileSync('settings.json');
 let jitsi_url = JSON.parse(settings_json)["jitsi_url"];
 let discord_url = JSON.parse(settings_json)["discord_url"];
+let twitch_url = JSON.parse(settings_json)["twitch_url"];
+let owncast_url = JSON.parse(settings_json)["owncast_url"];
+let owncast_chat = JSON.parse(settings_json)["owncast_chat"];
+let owncast_site_url = JSON.parse(settings_json)["owncast_site_url"];
 //console.log(jitsi_url)
 
 app.use("/css/", express.static('css'));
@@ -18,7 +22,11 @@ let lcd_text = "";
 app.get('/', function (req, res) {
     res.render("index" , {
         jitsi_url : jitsi_url,
-        discord_url: discord_url
+        discord_url: discord_url,
+        twitch_url: twitch_url,
+        owncast_url: owncast_url,
+        owncast_chat: owncast_chat,
+        owncast_site_url: owncast_site_url
     });
    //res.sendFile(__dirname + "/index.html");
 });
